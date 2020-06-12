42945cab7fff6b916b82c2574272a26ca0dd96b8
Nargonath <jonas.pauthier@gmail.com>
2020-06-12 13:32:57 +0200

    feat: 🎸 add nodejs server ipv4 to module output

--- modules/nodejs-server/output.tf
+++ modules/nodejs-server/output.tf
@@ -2,3 +2,8 @@ output "urn" {
   description = "The Uniform Resource Name of the NodeJS droplet"
   value       = digitalocean_droplet.node_server.urn
 }
+
+output "ipv4" {
+  description = "The droplet IP v4"
+  value       = digitalocean_droplet.node_server.ipv4_address
+}

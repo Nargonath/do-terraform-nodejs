1083614451d596bd02106674b95f40a4585ea948
Jonas Pauthier <jonas.pauthier@gmail.com>
2020-07-03 17:47:24 +0200

    feat: 🎸 add required versions for provider and terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_version = "= 0.12.28"
+  required_providers {
+    digitalocean = "~> 1.19"
+  }
+}
+
 provider "digitalocean" {
   token   = var.do_token
   version = "~> 1.19"
